575941fb732eaf1c5b1d96b56a9bace7c57658bf
Michaverkh <michawerkh@gmail.com>
2021-09-11 14:13:14 +0300

    Работа с формами и svg

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -4,6 +4,9 @@ const navToggle = document.querySelector('.main-nav__toggle');
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
+
+    console.log('кнопка');
+
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
